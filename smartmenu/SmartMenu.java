0d2ad135ba69fbc8e677a9a7031f07e9ce135db7
AbdelrahmanEssamA <41570565+AbdelrahmanEssamA@users.noreply.github.com>
2019-02-19 00:11:44 +0200

    Create SmartMenu.java

---
+++ smartmenu/SmartMenu.java
@@ -0,0 +1,49 @@
+package smartmenu;
+import java.sql.*;
+import smartmenu.MainClasses.Item;
+
+import smartmenu.PersonClasses.*;
+public class SmartMenu {
+    public static void main(String[] args) throws SQLException {
+        /*
+        Staff s =  new Staff("Sara","123","Sara Sami", "01559213386", "Female","day");
+        DatabaseController.WriteObject("Person", s.getID(), s);
+        Staff x =  new Staff("karim","123","Karim Ahmed", "01254334384", "Male","night");
+        DatabaseController.WriteObject("Person", x.getID(), x);
+        Customer c = new Customer("abdo","123","Abdelrahman Essam","01009948480","Male");
+        DatabaseController.WriteObject("Person", c.getID(), c);
+        c = new Customer("ali","123","Mahmoud Sami","01159216686","Male");
+        DatabaseController.WriteObject("Person", c.getID(), c);
+        c = new Customer("Maha","123","Maha Mohamed","01149948490","Female");
+        DatabaseController.WriteObject("Person", c.getID(), c);
+        Item item = new Item("Coffe",12.26,"beverages");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Tea",10.26,"beverages");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Iced Coffe",25.39,"beverages");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Candy Shake",33.51,"beverages");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Caramel",20.26,"beverages");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Pepsi",7.00,"beverages");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Cheese Burger",30.56,"Burger");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Strawberry Donut",22.39,"Desert");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Donut box",123.51,"Desert");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Donut box",123.51,"Desert");
+        item = new Item("BBQ Burger",40.56,"Burger");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Black Burger",72.39,"Burger");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Bacon Cheese Burger",63.51,"Burger");
+        item = new Item("Donut",17.56,"Desert");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        item = new Item("Choclate Donut",20.56,"Desert");
+        DatabaseController.WriteObject("Item",item.generateID(),item);
+        */
+    }
+}
